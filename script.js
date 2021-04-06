919626e17c0b47aace08903770698a7a1e7d7ebb
Tyler Moroz <moroztyler@gmail.com>
2021-04-06 15:05:46 -0600

    adding computerPlay() function

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let compChoice = ["Rock", "Paper", "Scissors"];
+    console.log(compChoice[Math.floor(Math.random() * compChoice.length)]);
+}
+
+computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    
+}
