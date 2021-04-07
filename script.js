1167dbc0d8a045e1cd89573e1f80014d64051b1d
Tyler Moroz <moroztyler@gmail.com>
2021-04-06 18:49:14 -0600

    added a playRound() function into the  game() function with a for loop to play 5 rounds and declare a winner

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 function computerPlay() {
     let compChoice = ["Rock", "Paper", "Scissors"];
-    console.log(compChoice[Math.floor(Math.random() * compChoice.length)]);
+    let result = (compChoice[Math.floor(Math.random() * compChoice.length)]);
+    return result;
 }
-
 computerPlay();
 
-function playRound(playerSelection, computerSelection) {
-    
+function game() {
+    let player = 0;
+    let computer = 0;
+
+    for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt('What is your move?', '');
+    const computerSelection = computerPlay();
+
+    function playRound(playerSelection, computerSelection) {
+        let str = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+        if (str === 'Rock' && computerSelection === 'Paper') {
+            computer += 1;
+            return "You Lose! Paper beats Rock." + " computer: " + computer + " player: " + player;
+        } else if (str === 'Rock' && computerSelection === 'Scissors') {
+            player += 1;
+            return "You Win! Rock beats Scissors." + " computer: " + computer + " player: " + player;
+        } else if (str === 'Paper' && computerSelection === 'Scissors') {
+            computer += 1;
+            return "You Lose! Scissors beats Paper." + " computer: " + computer + " player: " + player;
+        } else if (str === 'Paper' && computerSelection === 'Rock') {
+            player += 1;
+            return "You Win! Paper beats Rock." + " computer: " + computer + " player: " + player;
+        } else if (str === 'Scissors' && computerSelection === 'Rock') {
+            computer += 1;
+            return "You Lose! Rock beats Scissors." + " computer: " + computer + " player: " + player;
+        } else if (str === 'Scissors' && computerSelection === 'Paper') {
+            player += 1;
+            return "You Win! Scissors beats Paper." + " computer: " + computer + " player: " + player;
+        } else {
+            return "Tie Round! Play again." + " computer: " + computer + " player: " + player;
+        }
+    }
+        console.log(playRound(playerSelection, computerSelection));
 }
+    if (player < computer) {
+        alert("Computer wins the game! " + computer + "-" + player);
+    } else {
+        alert("Player wins the game! " + player + "-" + computer);
+    }
+}
+
+game();
