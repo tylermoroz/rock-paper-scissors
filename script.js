8ea3f6b5267bc3754a7ff74a4231d6c69aa49cda
Tyler Moroz <moroztyler@gmail.com>
2021-04-07 16:07:34 -0600

    changed for loop to do  while loop to allow game to declare a winner after 5 rounds won instead of 5 rounds played and added a delcareWinner() function to alert the player of who won

--- script.js
+++ script.js
@@ -1,52 +1,45 @@
+let compChoice = ["Rock", "Paper", "Scissors"];
+
 function computerPlay() {
-    let compChoice = ["Rock", "Paper", "Scissors"];
     let result = (compChoice[Math.floor(Math.random() * compChoice.length)]);
     return result;
 }
 computerPlay();
 
+
 function game() {
     let player = 0;
     let computer = 0;
 
-    for (let i = 0; i < 5; i++) {
+    do {
     const playerSelection = prompt('What is your move?', '');
     const computerSelection = computerPlay();
 
     function playRound(playerSelection, computerSelection) {
         let str = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-        if (str === 'Rock' && computerSelection === 'Paper') {
+        if (str === 'Rock' && computerSelection === 'Paper' || str === 'Paper' && computerSelection === 'Scissors' || str === 'Scissors' && computerSelection === 'Rock') {
             computer += 1;
-            return "You Lose! Paper beats Rock." + " computer: " + computer + " player: " + player;
-        } else if (str === 'Rock' && computerSelection === 'Scissors') {
+            return `You Lose! ${computerSelection} beats ${str}. computer: ${computer} player: ${player}`;
+        } else if (str === 'Rock' && computerSelection === 'Scissors' || str === 'Paper' && computerSelection === 'Rock' || str === 'Scissors' && computerSelection === 'Paper') {
             player += 1;
-            return "You Win! Rock beats Scissors." + " computer: " + computer + " player: " + player;
-        } else if (str === 'Paper' && computerSelection === 'Scissors') {
-            computer += 1;
-            return "You Lose! Scissors beats Paper." + " computer: " + computer + " player: " + player;
-        } else if (str === 'Paper' && computerSelection === 'Rock') {
-            player += 1;
-            return "You Win! Paper beats Rock." + " computer: " + computer + " player: " + player;
-        } else if (str === 'Scissors' && computerSelection === 'Rock') {
-            computer += 1;
-            return "You Lose! Rock beats Scissors." + " computer: " + computer + " player: " + player;
-        } else if (str === 'Scissors' && computerSelection === 'Paper') {
-            player += 1;
-            return "You Win! Scissors beats Paper." + " computer: " + computer + " player: " + player;
+            return `You Win! ${str} beats ${computerSelection}. computer: ${computer} player: ${player}`;
         } else {
-            return "Tie Round! Play again." + " computer: " + computer + " player: " + player;
+            return `Tie Round! computer: ${computer} player: ${player}`;
         }
     }
         console.log(playRound(playerSelection, computerSelection));
-}
-    if (player < computer) {
-        alert("Computer wins the game! " + computer + "-" + player);
-    } else if (player > computer) {
-        alert("Player wins the game! " + player + "-" + computer);
-    } else {
-        alert("Tie game! " + player + "-" + computer);
+} while (computer < 5 && player < 5)
+
+    function winner() {
+        if (computer === 5) {
+            alert(`Computer wins the game! ${computer} - ${player}`);
+        } else if (player === 5) {
+            alert(`Player wins the game! ${player} - ${computer}`);
+        }
     }
+    winner();    
+
 }
 
 game();
